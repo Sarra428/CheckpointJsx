1d1c79706d536cfd757a210970c55ab607ef8535
sarra <sarrah.aouina@gmail.com>
2021-12-10 13:53:41 +0100

    checkpoint jsx

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import jsx2 from '../src/image/jsx2.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ 
+
+<div className='title'>
+
+      <h1 className='titlered'>Your name here</h1> <br/> 
+
+            <img src={jsx2}></img>
+
+            <img src="./jsx1.png"></img>
+
+</div>
+
+<div className="video">
+
+<video src="videojsx.mp4"></video>
+
+</div>
+
+
+
+
+
     </div>
   );
 }
